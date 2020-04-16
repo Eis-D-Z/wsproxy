c4c689fb04f84f0842ad8e6d1f33ddd96a9fad2b
Eis <monoatomic.alex@gmail.com>
2020-04-16 13:56:51 +0300

    Added multiple client capability

--- wsproxy.py
+++ wsproxy.py
@@ -3,23 +3,37 @@ import asyncio
 import signal
 import logging
 import os
+from urllib.parse import unquote
 
 class WebsocketProxy():
     logger = logging.getLogger('websockets')
     logger.setLevel(logging.INFO)
-    
+
+    def __init__(self):
+        self.connections = {}
+
     async def producer_handler(self, server, path):
         while True:
-            message = await self.client.recv()
+            message = await self.connections[server].recv()
             self.logger.info(message)
             await server.send(message)
     
     async def consumer_handler(self, server, path):
         async for message in server:
             self.logger.info(message)
-            await self.client.send(message)
+            await self.connections[server].send(message)
+    
+    async def register(self, websocket, url):
+        client = await websockets.connect(url)
+        self.connections[websocket] = client
+    
+    async def unregister(self, websocket):
+        await self.connection[websocket].close()
+        del self.connections[websocket]
 
     async def handler(self, websocket, path):
+        url = unquote(path.split("?")[1].split("=")[1])
+        await self.register(websocket, url)
         try:
             consumer_task = asyncio.ensure_future(self.consumer_handler(websocket, path))
             producer_task = asyncio.ensure_future(self.producer_handler(websocket, path))
@@ -29,21 +43,24 @@ class WebsocketProxy():
             for task in pending:
                 task.cancel()
         finally:
-            self.stop()
+           await self.unregister(websocket)
 
-    def stop(self):
-        pid = os.getpid()
-        os.kill(pid, signal.SIGTERM)
+    async def stop(self):
+        await self.client.close()
+        #pid = os.getpid()
+        #os.kill(pid, signal.SIGTERM)
 
-    async def server(self, stop, websocket_url):
-        async with websockets.serve(self.handler,"192.168.2.3", 8764) as server:
+    async def server(self, stop):
+        async with websockets.serve(self.handler,"127.0.0.1", 8764) as server:
             self.server = server
-            async with websockets.connect(websocket_url) as client:
-                self.client = client
-                await stop
+            await stop
 
-    def start(self, websocket_url="wss://echo.websocket.org/"):
-        loop = asyncio.get_event_loop()
-        stop = loop.create_future()
-        loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
-        loop.run_until_complete(self.server(stop,websocket_url))
+
+if __name__ == "__main__":
+
+    proxy = WebsocketProxy()
+    loop = asyncio.get_event_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+    loop.run_until_complete(proxy.server(stop))
+    loop.run_forever()
